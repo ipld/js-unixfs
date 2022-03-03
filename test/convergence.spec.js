1ce063b8505964a77ab41a9950b64d0a521b2493
Irakli Gozalishvili <contact@gozala.io>
2022-03-02 16:41:40 -0800

    chore: add dagger tests

--- test/convergence.spec.js
+++ test/convergence.spec.js
@@ -9,17 +9,14 @@ import * as FS from "fs"
  * @param {import('./matrix').Input} input
  */
 const createTest = input =>
-  async function () {
+  /**
+   * @this {{timeout(ms:number):void}}
+   */
+  async function test() {
     this.timeout(30000)
     const config = await parseConfig(input)
     const { writer, blocks } = FileImporter.createImporter({}, config)
     const file = await unpackFile(config.url)
-
-    // console.log(">> file", file.size)
-    // const stream = await FS.createReadStream("./large_repeat_1GiB")
-    // for await (const slice of stream) {
-    //   writer.write(slice)
-    // }
 
     // @ts-expect-error - see https://github.com/DefinitelyTyped/DefinitelyTyped/pull/59057
     const stream = /** @type {ReadableStream<Uint8Array>} */ (file.stream())
@@ -31,16 +28,14 @@ const createTest = input =>
         assert.deepEqual(link.cid, config.cid)
         break
       } else {
-        console.log("<<", read.value.byteLength)
         writer.write(read.value)
       }
     }
   }
 
 describe("convergence tests", () => {
-  for (const input of Matrix.slice(0, 1)) {
+  for (const input of Matrix.slice(0, 10)) {
     const title = `${input.cmd} ${input.source}`
-    console.log(title)
 
     if (input.inlining > 0) {
       it.skip(title, createTest(input))
