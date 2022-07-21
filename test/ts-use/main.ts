03c69a6f09811cd72d38252824137590bc48e4ba
Irakli Gozalishvili <contact@gozala.io>
2022-07-21 12:44:38 +0200

    fix: types in the ts test

--- test/ts-use/main.ts
+++ test/ts-use/main.ts
@@ -1,5 +1,4 @@
 import * as UnixFS from "@ipld/unixfs"
-import * as UnixFSFile from "@ipld/unixfs/src/file.js"
 import * as Rabin from "@ipld/unixfs/src/file/chunker/rabin.js"
 import * as Fixed from "@ipld/unixfs/src/file/chunker/fixed.js"
 import * as Balanced from "@ipld/unixfs/src/file/layout/balanced.js"
@@ -12,8 +11,11 @@ export const test = async () => {
     content: new Uint8Array(32).fill(1),
   })
 
-  const { blocks, writer } = UnixFSFile.createImporter()
-  writer.write(new Uint8Array(32).fill(1))
-  const link = await writer.close()
+  const { blocks, writer } = UnixFS.create()
+  const file = writer.createFileWriter()
+  file.write(new Uint8Array(32).fill(1))
+  const link = await file.close()
   link.cid.toString()
+
+  writer.close()
 }
